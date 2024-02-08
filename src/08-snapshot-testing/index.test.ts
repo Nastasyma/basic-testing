49831256e492d8f0dcd7bddd6a2aae411cd6c5d8
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-02-08 15:34:52 +0300

    feat: implement 08-module tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,34 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const elements = [11, 22, 33, 44];
+    const list = {
+      value: 11,
+      next: {
+        value: 22,
+        next: {
+          value: 33,
+          next: {
+            value: 44,
+            next: {
+              value: null,
+              next: null,
+            },
+          },
+        },
+      },
+    };
+
+    expect(generateLinkedList(elements)).toStrictEqual(list);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elements = [44, 55, 66];
+    const list = generateLinkedList(elements);
+
+    expect(list).toMatchSnapshot();
   });
 });
